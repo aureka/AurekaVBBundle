acb2f1e36905e37a2e7507ffd5ac51dc1fe76809
Carles Climent <carlescliment@hotmail.com>
2014-04-24 16:32:29 +0200

    More factory refactorings

--- src/Aureka/VBBundle/Factory/ServicesFactory.php
+++ src/Aureka/VBBundle/Factory/ServicesFactory.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Aureka\VBBundle\VBConfiguration;
 use Aureka\VBBundle\Event\Listener\LoginListener;
-use Aureka\VBBundle\Handler\LogoutHandler;
 use Aureka\VBBundle\VBUsers;
 
 
@@ -16,15 +15,21 @@ class ServicesFactory
 
     public static function createLogin(VBConfiguration $config, RequestStack $request_stack)
     {
-        $user_provider = new VBUsers($config->createDB());
-        $request = $request_stack->getMasterRequest() ?: new Request();
-        return new LoginListener($user_provider, $config->createSession($request));
+        return self::create('Aureka\VBBundle\Event\Listener\LoginListener', $config, $request_stack);
     }
+
 
     public static function createLogout(VBConfiguration $config, RequestStack $request_stack)
     {
+        return self::create('Aureka\VBBundle\Handler\LogoutHandler', $config, $request_stack);
+    }
+
+
+    private static function create($class_name, VBConfiguration $config, RequestStack $request_stack)
+    {
+
         $user_provider = new VBUsers($config->createDB());
         $request = $request_stack->getMasterRequest() ?: new Request();
-        return new LogoutHandler($user_provider, $config->createSession($request));
+        return new $class_name($user_provider, $config->createSession($request));
     }
 }
